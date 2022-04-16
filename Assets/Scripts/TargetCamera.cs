879d6e32af1a3a96e17cd8fd76ddc60b9288af30
museekee <museekee123@gmail.com>
2022-04-17 00:35:49 +0900

    카메라 뒤 꽁무니

---
+++ Assets/Scripts/TargetCamera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TargetCamera : MonoBehaviour
+{
+    public GameObject Player;
+    public Vector3 offset;
+
+    void Update()
+    {
+        Rigidbody rb = Player.GetComponent<Rigidbody>();
+        Debug.Log(rb.rotation);
+        Vector3 FixedPos = 
+            new Vector3(
+                Player.transform.position.x + offset.x,
+                Player.transform.position.y + offset.y,
+                Player.transform.position.z + offset.z
+            );
+        transform.position = FixedPos;
+    }
+}
